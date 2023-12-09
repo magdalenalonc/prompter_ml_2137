7ddac07f45936d93e58a8572fa3b7d03ca75804c
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-12-09 23:47:14 +0100

    Add ask() method and refactor Prompter

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -4,22 +4,22 @@ import 'terminal.dart';
 final Terminal _terminal = const Terminal();
 
 class Prompter {
-  bool askBinary(String prompt) {
+  String? _ask(String prompt, List<Option> options) {
     _terminal.clearScreen();
-    _terminal.printPrompt('$prompt (y/n)');
-    _terminal.printOptions([]);
+    _terminal.printPrompt(prompt);
+    _terminal.printOptions(options);
 
-    final input = _terminal.collectInput();
+    return _terminal.collectInput();
+  }
+
+  bool askBinary(String prompt) {
+    final input = _ask('$prompt (y/n)', []);
 
     return input!.contains('y');
   }
 
   askMultiple(String prompt, List<Option> options) {
-    _terminal.clearScreen();
-    _terminal.printPrompt(prompt);
-    _terminal.printOptions(options);
-
-    final input = _terminal.collectInput();
+    final input = _ask(prompt, options);
 
     try {
       return options[int.parse(input!)].value;
