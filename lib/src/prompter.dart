24489a00d8f8b08cd5e3e839b6b6e403ac34001b
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-12-09 23:08:38 +0100

    Refactor Prompter

--- lib/src/prompter.dart
+++ lib/src/prompter.dart
@@ -1,12 +1,15 @@
 import 'option.dart';
 import 'terminal.dart';
 
-final Terminal terminal = const Terminal();
+final Terminal _terminal = const Terminal();
 
 class Prompter {
-
   ask(String prompt, List<Option> options) {
-    terminal.printPrompt(prompt);
-    terminal.printOptions(options);
+    _terminal.printPrompt(prompt);
+    _terminal.printOptions(options);
+
+    final input = _terminal.collectInput();
+
+    return options[int.parse(input!)].value;
   }
 }
